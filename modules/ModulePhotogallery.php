89f090459c53dd0066d9ad3aa8017656aec4d24a
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-12-19 19:49:17 +0330

    add ModulePhotogallery.php

---
+++ modules/ModulePhotogallery.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   photogallery
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL3
+ * @copyright respinar 2014
+ */
+
+
+/**
+ * Namespace
+ */
+namespace photogallery;
+
+
+/**
+ * Class ModulePhotogalleryDetail
+ *
+ * @copyright  respinar 2014
+ * @author     Hamid Abbaszadeh
+ * @package    Devtools
+ */
+class ModulePhotogallery extends \Module
+{
+
+
+}
